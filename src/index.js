f042854efb519b2cbdb2f0b837c84041415f62a7
farzadrabiee <farzad.rabiee@outlook.com>
2019-02-21 17:30:11 +1100

    Revert "remove card fron index"
    This reverts commit 1cf170eb1f5c101c073162449449c7382a9dab4f.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Card from './Card';
 import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
