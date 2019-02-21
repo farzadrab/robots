daea5a49430d8f759782918f51953535e4bb0b49
farzadrabiee <farzad.rabiee@outlook.com>
2019-02-21 17:46:07 +1100

    reformat cardlist

--- src/CardList.js
+++ src/CardList.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Card from './Card';
 
-const CardList = ({ robots }) => {
-    const cardComponent = robots.map((user, i)=>{
-       return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
-    });
+const CardList = ({robots}) => {
     return (
         <div>
-            {cardComponent}
+            {
+                robots.map((user, i) => {
+                    return (
+                        <Card
+                            key={robots[i].id}
+                            id={robots[i].id}
+                            name={robots[i].name}
+                            email={robots[i].email}
+                        />
+                    );
+                })
+            }
         </div>
     )
 };
